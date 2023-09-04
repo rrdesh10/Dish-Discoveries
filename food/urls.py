e2724df2b80ee820305e77ff82b6131406bfaf2f
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-09-04 12:46:00 +0530

    html styling added

--- food/urls.py
+++ food/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'food'
 urlpatterns = [
-    path('', views.index, name='index'), 
-    path('<int:item_id>/', views.detail, name='detail'),  #detail view 
-    path('add/', views.create, name='create'),  # add items
+    path('', views.IndexItem.as_view(), name='index'), 
+    path('<int:pk>/', views.DetailItem.as_view(), name='detail'),  #detail view 
+    path('add/', views.CreateItem.as_view(), name='create'),  # add items
     path('update/<int:id>/', views.update, name='update'),  # update items
     path('delete/<int:id>/', views.delete, name='delete'),  # delete items
 ]
--- food/views.py
+++ food/views.py
@@ -1,33 +1,41 @@
+from django.forms.models import BaseModelForm
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
+from django.views.generic import ListView, DetailView, CreateView
 
-# Create your views here.
 
-def index(request):
-    item_list = Item.objects.all()
-    context = {'item_list' : item_list,}
-    return render(request, 'food/index.html', context)
+class IndexItem(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 def item(request):
     return HttpResponse("Hello world")
 
-def detail(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    context = {
-        'item' : item,
-    }
-    return render(request, 'food/detail.html', context)
 
-def create(request):
-    form = ItemForm(request.POST or None)
+class DetailItem(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
 
-    if form.is_valid():
-        form.save()
-        return redirect('food:index')
+# def create(request):
+#     form = ItemForm(request.POST or None)
+
+#     if form.is_valid():
+#         form.save()
+#         return redirect('food:index')
     
-    return render(request, 'food/item-form.html', {'form': form})
+#     return render(request, 'food/item-form.html', {'form': form})
+
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
 
 
 def update(request, id):
